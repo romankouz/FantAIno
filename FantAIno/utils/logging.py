9245517f4082325486ec6cf6da6c9e4b179e295d
romankouz <roko@umich.edu>
2026-04-10 15:44:49 -0400

    embeddings

--- FantAIno/utils/logging.py
+++ FantAIno/utils/logging.py
@@ -1,11 +1,13 @@
 import logging
 import os
 
+from FantAIno.constants import LOGS_DIR
+
 def create_logger(logger_name: str, log_file_name: str, logging_level: int = logging.DEBUG) -> logging.Logger:
     """Creates a logger for the Fantaino project."""
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging_level)
-    handler = logging.FileHandler(os.path.join('logs', log_file_name), encoding='utf-8')
+    handler = logging.FileHandler(os.path.join(LOGS_DIR, log_file_name), encoding='utf-8')
     logger.addHandler(handler)
     logger.propagate = False # perhaps remove?
     return logger
