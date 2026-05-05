aaa2915a43c3374ddf8365afe80a358960024f6a
romankouz <roko@umich.edu>
2026-05-04 21:17:57 -0400

    logger fix for streamlit

--- FantAIno/utils/logging.py
+++ FantAIno/utils/logging.py
@@ -1,13 +1,23 @@
 import logging
 import os
+import streamlit as st
 
 from FantAIno.constants import LOGS_DIR
 
 def create_logger(logger_name: str, log_file_name: str, logging_level: int = logging.DEBUG) -> logging.Logger:
     """Creates a logger for the Fantaino project."""
+
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging_level)
-    handler = logging.FileHandler(os.path.join(LOGS_DIR, log_file_name), encoding='utf-8')
+
+    is_cloud = "OPENAI_API_KEY" in st.secrets
+
+    if not is_cloud and log_file_name:
+        handler = logging.FileHandler(os.path.join(LOGS_DIR, log_file_name), encoding='utf-8')
+    else:
+        handler = logging.StreamHandler()
+
     logger.addHandler(handler)
     logger.propagate = False # perhaps remove?
+    
     return logger
