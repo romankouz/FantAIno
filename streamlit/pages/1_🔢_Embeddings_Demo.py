87471442466ca97a2497592c589612ed8173b83d
romankouz <roko@umich.edu>
2026-05-05 01:03:30 -0400

    try s3 backslash pt 2

--- streamlit/pages/1_🔢_Embeddings_Demo.py
+++ streamlit/pages/1_🔢_Embeddings_Demo.py
@@ -198,7 +198,7 @@ with col2:
         lyrics_filename = sanitize_filename(f"{artist}___{album}.jsonl")
         lyrics_response = s3_client.get_object(
             Bucket=S3_GENERAL_PURPOSE_BUCKET_NAME, 
-            Key=os.path.join("lyrics", lyrics_filename)
+            Key=f"lyrics\\{lyrics_filename}"
         )
         logic_no_pressure = json.load(lyrics_response['Body'])
 
@@ -206,7 +206,7 @@ with col2:
         lyrics_filename = sanitize_filename(f"{artist}___{album}.jsonl")
         lyrics_response = s3_client.get_object(
             Bucket=S3_GENERAL_PURPOSE_BUCKET_NAME, 
-            Key=os.path.join("lyrics", lyrics_filename)
+            Key=f"lyrics\\{lyrics_filename}"
         )
         madonna_ray_of_light = json.load(lyrics_response['Body'])
    
